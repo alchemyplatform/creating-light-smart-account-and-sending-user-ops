a9f62e395ced641b8de6c05790d071ecce0cc1d4
Al Luken <al5031@nyu.edu>
2023-10-24 14:02:02 -0700

    latest

---
+++ helpers/createProvider.ts
@@ -0,0 +1,41 @@
+import {
+  LightSmartContractAccount,
+  getDefaultLightAccountFactory,
+} from "@alchemy/aa-accounts";
+import { AlchemyProvider } from "@alchemy/aa-alchemy";
+import { LocalAccountSigner, SmartAccountSigner } from "@alchemy/aa-core";
+import * as dotenv from "dotenv";
+import { sepolia } from "viem/chains";
+dotenv.config();
+
+const PRIV_KEY = process.env.PRIV_KEY!;
+const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL!;
+
+const ENTRYPOINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
+
+export default async function createSigner() {
+  const chain = sepolia;
+  const PRIVATE_KEY = PRIV_KEY; // Replace with the private key of your EOA that will be the owner of Light Account
+
+  const eoaSigner: SmartAccountSigner =
+    LocalAccountSigner.privateKeyToAccountSigner(PRIVATE_KEY as `0x${string}`); // Create a signer for your EOA
+
+  // Create a provider with your EOA as the smart account owner, this provider is used to send user operations from your smart account and interact with the blockchain
+  const provider = new AlchemyProvider({
+    apiKey: ALCHEMY_API_URL, // or replace with your Alchemy API key, you can get one at https://dashboard.alchemy.com/
+    chain,
+    // Entrypoint address, you can use a different entrypoint if needed, check out https://docs.alchemy.com/reference/eth-supportedentrypoints for all the supported entrypoints
+    entryPointAddress: ENTRYPOINT_ADDRESS,
+  }).connect(
+    (rpcClient) =>
+      new LightSmartContractAccount({
+        entryPointAddress: ENTRYPOINT_ADDRESS,
+        chain: rpcClient.chain,
+        owner: eoaSigner,
+        factoryAddress: getDefaultLightAccountFactory(rpcClient.chain), // Default address for Light Account on Sepolia, you can replace it with your own.
+        rpcClient,
+      })
+  );
+
+  return provider;
+}
---
+++ scripts/sendEthToSca.ts
@@ -0,0 +1,31 @@
+import * as dotenv from "dotenv";
+import { createWalletClient, http, parseEther } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { sepolia } from "viem/chains";
+dotenv.config();
+
+import { counterfactualAddress } from "../accountInfo.json";
+
+const PRIV_KEY = process.env.PRIV_KEY!;
+const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL!;
+
+async function main() {
+  const account = privateKeyToAccount(`0x${PRIV_KEY}`);
+
+  const wallet = createWalletClient({
+    account: account,
+    chain: sepolia,
+    transport: http(ALCHEMY_API_URL),
+  });
+
+  const txHash = await wallet.sendTransaction({
+    to: counterfactualAddress as `0x${string}`,
+    value: parseEther("0.5"),
+  });
+
+  return txHash;
+}
+
+main().then((txHash) => {
+  console.log("Transation hash: ", txHash);
+});
